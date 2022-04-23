63e8f8560e3be378743a73639470f2708e9164e0
Fanoa <91606197+uvsq22105665@users.noreply.github.com>
2022-04-23 16:23:22 +0200

    création et remplissage des tables

--- requetesSQL.sql
+++ requetesSQL.sql
@@ -12,10 +12,8 @@ INSERT INTO DimActor (star) SELECT distinct star FROM MovieData ;
 Create table DimCompany
 (
 IdCompany int primary key identity,
-company text
+company varchar(max)
 )
-insert into DimCompany(company) SELECT DISTINCT CAST(company AS VARCHAR(MAX)) AS company FROM MovieData
-
 
 Create table DimDirector
 (
@@ -34,7 +32,7 @@ INSERT INTO DimWriter(writer) SELECT distinct writer FROM MovieData ;
 Create table DimMovie
 (
 IdMovie int primary key identity,
-nameMovie text,
+nameMovie varchar(max),
 genre varchar(50),
 yearMovie varchar(50),
 released varchar(50),
@@ -62,6 +60,12 @@ budget float,
 runtime float
 )
 
+/** Remplir la table des faits **/ 
+
+insert into FactMovie(idMovie, IdActor, IdCompany, IdDirector, IdWriter, score, vote, gross, budget) 
+select idMovie, IdActor, IdCompany, IdDirector, IdWriter, score, vote, gross, budget
+from MovieData
+
 /************************************************************************
  * table qui regroupe les données pour remplir la table de fait
  ************************************************************************
@@ -77,8 +81,8 @@ IdWriter int ,
 director varchar(50),
 writer varchar(50),
 star varchar(50),
-company text,
-nameMovie text,
+company varchar(max),
+nameMovie varchar(max),
 genre varchar(50),
 yearMovie varchar(50),
 released varchar(50),
@@ -95,13 +99,18 @@ runtime float
 UPDATE MovieData
 SET IdActor = (select IdActor from DimActor where MovieData.star = DimActor.star)
 
+/**Remplit IdMovie**/
+UPDATE MovieData
+SET IdMovie = (select IdMovie from DimMovie where MovieData.nameMovie = DimMovie.nameMovie and MovieData.yearMovie = DimMovie.yearMovie)
+
+/**Remplit IdCompany**/
+UPDATE MovieData
+SET IdCompany = (select IdCompany from DimCompany where MovieData.company = DimMovie.company)
+
 /**Remplit IdDirector**/
 UPDATE MovieData
 SET IdDirector = (select IdDirector from DimDirector where MovieData.director = DimDirector.director)
 
-
 /**Remplit IdWriter**/
 UPDATE MovieData
 SET IdWriter = (select IdWriter from DimWriter where MovieData.writer = DimWriter.writer)
-
-
